2ae75a20d4980676adea67d0650cb14f51912f13
anguy22 <85816020+anguy22@users.noreply.github.com>
2026-05-12 23:51:56 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -580,7 +580,7 @@
     }
 
     ctx.save();
-    ctx.strokeStyle = "rgba(226,232,255," + alpha.toFixed(3) + ")";
+    ctx.strokeStyle = "rgba(0,0,0," + alpha.toFixed(3) + ")";
     ctx.lineWidth = 2.9;
     ctx.setLineDash([4, 6]);
     ctx.shadowBlur = 0;
